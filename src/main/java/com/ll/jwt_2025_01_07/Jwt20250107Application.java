7205f70c12d6e1ed0ca2d0d22305f52a8729c505
paek1127 <paek1127@gmail.com>
2025-01-29 19:28:25 +0900

    base setting

--- src/main/java/com/ll/jwt_2025_01_07/Jwt20250107Application.java
+++ src/main/java/com/ll/jwt_2025_01_07/Jwt20250107Application.java
@@ -2,8 +2,10 @@ package com.ll.jwt_2025_01_07;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Jwt20250107Application {
 
     public static void main(String[] args) {
